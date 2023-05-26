b1bd41f5779bbba7d591644743d2cb484b580e84
jrtaylor71 <42515735+jrtaylor71@users.noreply.github.com>
2023-05-26 12:59:01 -0500

    Update decimal-patch.sh
    Correct path's to scripts.

--- decimal-patch.sh
+++ decimal-patch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo patch scripts/bme280F.py bme280F.patch
+sudo patch /home/pi/scripts/bme280F.py bme280F.patch
 
-sudo cp scripts/bme280F.py scripts/bme280.py
+sudo cp /home/pi/scripts/bme280F.py /home/pi/scripts/bme280.py
 
 sudo patch /etc/openhab2/sitemaps/default.sitemap default.sitemap.patch
 
 sudo patch /var/lib/openhab2/jsondb/org.eclipse.smarthome.core.items.Item.json org.eclipse.smarthome.core.items.Item.json.patch
 
-sudo patch scripts/oneui/js/app.8debb52b.js app.8debb52b.js.patch
+sudo patch /home/pi/scripts/oneui/js/app.8debb52b.js app.8debb52b.js.patch
 
 sudo reboot
 
